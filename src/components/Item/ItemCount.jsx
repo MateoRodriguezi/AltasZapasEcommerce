b36046e0cd25f06697a4b9f71868089e7c1d2a62
Mateo Rodriguez <mrodriguez@prometeoapi.com>
2023-02-07 01:34:04 -0300

    se agrega intercambiabilidad en itemcount

--- src/components/Item/ItemCount.jsx
+++ src/components/Item/ItemCount.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { FaShoppingCart, FaRegLaughWink } from "react-icons/fa";
 
-const ItemCount = ({ addToCart }) => {
+
+const ItemCount = ({ }) => {
   const [count, setCount] = useState(0);
+  const [inputType, setInputType ] = useState('buttons');
 
   const handleClick = (value) => {
     if (count + value >= 0) {
@@ -11,24 +15,39 @@ const ItemCount = ({ addToCart }) => {
   };
 
   const handleAddToCart = () => {
-    addToCart(count);
-    setCount(0);
+    setInputType('links');
   };
 
   return (
     <div className="d-flex mt-3">
-      <button className="btn btn-danger" onClick={() => handleClick(-1)}>
-        -
-      </button>
-      <p className="m-0 font-weight-bold" style={{ fontSize: "1.5em", width: "50px", textAlign: "center" }}>{count}</p>
-      <button className="btn btn-success" onClick={() => handleClick(1)}>
-        +
-      </button>
-      <button className="btn btn-dark ml-3" onClick={handleAddToCart}>
-        Agregar al carrito
-      </button>
+      {inputType === 'buttons' ? (
+        <>
+          <button className="btn btn-danger" onClick={() => handleClick(-1)}>
+            -
+          </button>
+          <p className="m-0 font-weight-bold" style={{ fontSize: "1.5em", width: "50px", textAlign: "center" }}>
+            {count}
+          </p>
+          <button className="btn btn-success" onClick={() => handleClick(1)}>
+            +
+          </button>
+          <button className="btn btn-dark ml-3" onClick={handleAddToCart}>
+            Agregar al carrito <FaShoppingCart />
+          </button>
+        </>
+      ) : (
+        <>
+          <Link to="/cart">
+            <button className="btn btn-dark ml-3"> Ir al carrito <FaShoppingCart />  </button>
+          </Link>
+          <Link to="/">
+            <button className="btn btn-dark ml-3">Seguir comprando  <FaRegLaughWink />  </button>
+          </Link>
+        </>
+      )}
     </div>
   );
+  
 };
 
 export default ItemCount;
